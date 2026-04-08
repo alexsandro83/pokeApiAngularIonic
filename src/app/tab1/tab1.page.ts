e8a9564ba143fac78e1304bbf978db3fda87b6a5
alexsandro83 <alexsander83@hotmail.com>
2026-04-08 03:03:06 -0300

    primeira versão

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class Tab1Page {
 
-  constructor() {}
+  pokemons: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ionViewWillEnter() {
+    this.carregarPokemons();
+  }
+
+  carregarPokemons() {
+    this.http.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+      .subscribe((res: any) => {
+        this.pokemons = res.results;
+      });
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
 export class Tab2Page {
 
-  constructor() {}
+  nome: string = '';
+  pokemon: any;
+
+  constructor(private http: HttpClient) {}
+
+  buscar() {
+    const url = `https://pokeapi.co/api/v2/pokemon/${this.nome.toLowerCase()}`;
+
+    this.http.get(url).subscribe({
+      next: (res) => this.pokemon = res,
+      error: () => alert('Pokémon não encontrado')
+    });
+  }
 
 }
